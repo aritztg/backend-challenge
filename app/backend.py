48efbcf34f0b43d7b66e80c3a784bb6eeb5a946c
Aritz Tusell <aritz.tusell@otainsight.com>
2023-06-05 01:11:09 +0200

    Fix entry point port so it matches with README.md

--- app/backend.py
+++ app/backend.py
@@ -56,4 +56,4 @@ async def send_message(csrf_token: Annotated[str, Depends(check_token)],  # pyli
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=80)
